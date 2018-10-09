0a37c53f50e857afc6e4988dd602637f8af88367
delasalle-sio-derrien-r <delasalle.sio.derrien.r@gmail.com>
2018-10-09 16:05:45 +0200

    sdfsdf

--- test.php
+++ test.php
@@ -2,10 +2,10 @@
 // création de test.php 16:00 09/10/2018
 
 
-<<<<<<< HEAD
-echo "<text>TraceGPSoui</text><br>"
-=======
+
+echo "<text>TraceGPSoui</text><br>";
+
 echo "<text><b>TraceGPS</b></text><br>"
->>>>>>> branch 'master' of https://github.com/delasalle-sio-mentec-o/tracegps.git
+
 
 ?>
