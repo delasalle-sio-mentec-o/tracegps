bea220c9079f8270654ebbe43257e43a5e4ef744
delasalle-sio-couillard-t <couillard.sio.t@gmail.com>
2018-10-16 14:59:58 +0200

    avec le test en plus

--- modele/DAO.test.php
+++ modele/DAO.test.php
@@ -184,7 +184,7 @@ else {
 
 // Le code restant à développer va être réparti entre les membres de l'équipe de développement.
 // Afin de limiter les conflits avec GitHub, il est décidé d'attribuer une zone de ce fichier à chaque développeur.
-// Développeur 1 : lignes 200 à 299
+// théo le boss : lignes 200 à 299
 // Développeur 2 : lignes 300 à 399
 // Développeur 3 : lignes 400 à 500
 
@@ -198,9 +198,30 @@ else {
 
 
 // --------------------------------------------------------------------------------------
-// début de la zone attribuée au développeur 1 (xxxxxxxxxxxxxxxxxxxx) : lignes 200 à 299
+// début de la zone attribuée au développeur 1 (théo le boss ) : lignes 200 à 299
 // --------------------------------------------------------------------------------------
 
+// test de la méthode creerUneTrace ----------------------------------------------------------
+// modifié par Jim le 14/8/2018
+echo "<h3>Test de creerUneTrace : </h3>";
+$trace1 = new Trace(0, "2017-12-18 14:00:00", "2017-12-18 14:10:00", true, 3);
+$ok = $dao->creerUneTrace($trace1);
+if ($ok) {
+    echo "<p>Trace bien enregistrée !</p>";
+    echo $trace1->toString();
+}
+else {
+    echo "<p>Echec lors de l'enregistrement de la trace !</p>";
+}
+$trace2 = new Trace(0, date('Y-m-d H:i:s', time()), null, false, 3);
+$ok = $dao->creerUneTrace($trace2);
+if ($ok) {
+    echo "<p>Trace bien enregistrée !</p>";
+    echo $trace2->toString();
+}
+else {
+    echo "<p>Echec lors de l'enregistrement de la trace !</p>";
+}
 
 
 
