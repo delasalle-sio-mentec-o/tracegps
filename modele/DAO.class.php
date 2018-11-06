5518a80ba7e5ab4923f1d8f4d2628602ba13083a
delasalle-sio-colleu-e <colleu.e@i12-20.sio.local>
2018-11-06 14:03:31 +0100

    function getLesUtilisateursAutorisant($idUtilisateur) ok !

--- modele/DAO.class.php
+++ modele/DAO.class.php
@@ -603,49 +603,45 @@ class DAO
     public function getLesUtilisateursAutorisant($idUtilisateur) 
     {
         // préparation de la requête de recherche
-        $reqIdAutorisant = "select idAutorisant from tracegps_autorisations where idAutorise = :idUtilisateur;";
-        
+        $IdAutorisant = "select idAutorisant from tracegps_autorisations where idAutorise = :idUtilisateur;";
+        $reqIdAutorisant = $this->cnx->prepare($IdAutorisant);
         // liaison de la requête et de ses paramètres
-        $reqIdAutorisant->bindValue("idUtilisateur", $idUtilisateur, PDO::PARAM_STR);
+        $reqIdAutorisant->bindValue("idUtilisateur", $idUtilisateur, PDO::PARAM_INT);
         // exécution de la requête
-        $reqIdAutorisant->execute();
+        $reqIdAutorisant->execute();               
         
         $uneLigneIdAutorisant = $reqIdAutorisant->fetch(PDO::FETCH_OBJ);
+        
+        // construction d'une collection d'objets Utilisateur
+        $lesUtilisateursAutorisant = new ArrayObject();
         
         // tant qu'une ligne est trouvée :
         while ($uneLigneIdAutorisant) 
         {
-            $reqUtilisateursAutorisant = "select id, pseudo, mdpSha1, adrMail, numTel, niveau, dateCreation, nbTraces, dateDerniereTrace from tracegps_vue_utilisateurs where id = :idAutorisant;";
-            $reqUtilisateursAutorisant->bindValue("idAutorisant", $uneLigneIdAutorisant, PDO::PARAM_STR);
+            $UtilisateursAutorisant = "select id, pseudo, mdpSha1, adrMail, numTel, niveau, dateCreation, nbTraces, dateDerniereTrace from tracegps_vue_utilisateurs where id = :idAutorisant;";
+            $reqUtilisateursAutorisant = $this->cnx->prepare($UtilisateursAutorisant);
+            $reqUtilisateursAutorisant->bindValue("idAutorisant", $uneLigneIdAutorisant->idAutorisant, PDO::PARAM_INT);
             // extraction des données
-            $reqUtilisateursAutorisant->execute();
-            
-            $uneLigneUtilisateursAutorisant = $reqUtilisateursAutorisant->fetch(PDO::FETCH_OBJ);
-            
-            // construction d'une collection d'objets Utilisateur
-            $lesUtilisateursAutorisant = array();
-            // tant qu'une ligne est trouvée :
-            while ($uneLigneUtilisateursAutorisant)
-            {
-                // création d'un objet Utilisateur
-                $unId = utf8_encode($uneLigneUtilisateursAutorisant->id);
-                $unPseudo = utf8_encode($uneLigneUtilisateursAutorisant->pseudo);
-                $unMdpSha1 = utf8_encode($uneLigneUtilisateursAutorisant->mdpSha1);
-                $uneAdrMail = utf8_encode($uneLigneUtilisateursAutorisant->adrMail);
-                $unNumTel = utf8_encode($uneLigneUtilisateursAutorisant->numTel);
-                $unNiveau = utf8_encode($uneLigneUtilisateursAutorisant->niveau);
-                $uneDateCreation = utf8_encode($uneLigneUtilisateursAutorisant->dateCreation);
-                $unNbTraces = utf8_encode($uneLigneUtilisateursAutorisant->nbTraces);
-                $uneDateDerniereTrace = utf8_encode($uneLigneUtilisateursAutorisant->dateDerniereTrace);
+            $reqUtilisateursAutorisant->execute();            
+                       
+            $uneLigneUtilisateursAutorisant = $reqUtilisateursAutorisant->fetch(PDO::FETCH_OBJ);     
+
+            // création d'un objet Utilisateur
+            $unId = utf8_encode($uneLigneUtilisateursAutorisant->id);
+            $unPseudo = utf8_encode($uneLigneUtilisateursAutorisant->pseudo);
+            $unMdpSha1 = utf8_encode($uneLigneUtilisateursAutorisant->mdpSha1);
+            $uneAdrMail = utf8_encode($uneLigneUtilisateursAutorisant->adrMail);
+            $unNumTel = utf8_encode($uneLigneUtilisateursAutorisant->numTel);
+            $unNiveau = utf8_encode($uneLigneUtilisateursAutorisant->niveau);
+            $uneDateCreation = utf8_encode($uneLigneUtilisateursAutorisant->dateCreation);
+            $unNbTraces = utf8_encode($uneLigneUtilisateursAutorisant->nbTraces);
+            $uneDateDerniereTrace = utf8_encode($uneLigneUtilisateursAutorisant->dateDerniereTrace);
                 
-                $unUtilisateur = new Utilisateur($unId, $unPseudo, $unMdpSha1, $uneAdrMail, $unNumTel, $unNiveau, $uneDateCreation, $unNbTraces, $uneDateDerniereTrace);
-                // ajout de l'utilisateur à la collection
-                $lesUtilisateursAutorisant[] = $unUtilisateur;
-                // extrait la ligne suivante
-                $uneLigneUtilisateursAutorisant = $reqUtilisateursAutorisant->fetch(PDO::FETCH_OBJ);
-            }
-            // libère les ressources du jeu de données
-            $reqUtilisateursAutorisant->closeCursor();
+            $unUtilisateur = new Utilisateur($unId, $unPseudo, $unMdpSha1, $uneAdrMail, $unNumTel, $unNiveau, $uneDateCreation, $unNbTraces, $uneDateDerniereTrace);
+            // ajout de l'utilisateur à la collection
+            $lesUtilisateursAutorisant->append($unUtilisateur);
+            
+            $uneLigneIdAutorisant = $reqIdAutorisant->fetch(PDO::FETCH_OBJ);
         }
         // libère les ressources du jeu de données
         $reqIdAutorisant->closeCursor();
