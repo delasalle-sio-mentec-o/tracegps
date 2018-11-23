f98bc321d44ae298021dba1e7f6608ea18ad3139
delasalle-sio-mentec-o <delasalle.sio.mentec.o@gmail.com>
2018-11-23 11:38:36 +0100

    Ajout DemanderUneAutorisation.php

--- services/DemanderUneAutorisation.php
+++ services/DemanderUneAutorisation.php
@@ -28,24 +28,58 @@ $dao = new DAO();
 // la fonction $_REQUEST récupère par défaut le contenu des variables $_GET, $_POST, $_COOKIE
 if ( empty ($_REQUEST ["pseudo"]) == true) $pseudo = "";  else $pseudo = $_REQUEST ["pseudo"];
 if ( empty ($_REQUEST ["lang"]) == true) $lang = "";  else $lang = strtolower($_REQUEST ["lang"]);
+if ( empty ($_REQUEST ["mdpSha1"]) == true) $mdpSha1 = "";  else $mdpSha1 = strtolower($_REQUEST ["mdpSha1"]);
+if ( empty ($_REQUEST ["pseudoDestinataire"]) == true) $pseudoDestinataire = "";  else $pseudoDestinataire = strtolower($_REQUEST ["pseudoDestinataire"]);
+if ( empty ($_REQUEST ["nomPrenom"]) == true) $nomPrenom = "";  else $nomPrenom = strtolower($_REQUEST ["nomPrenom"]);
+if ( empty ($_REQUEST ["texteMessage"]) == true) $texteMessage = "";  else $texteMessage = strtolower($_REQUEST ["texteMessage"]);
 // "xml" par défaut si le paramètre lang est absent ou incorrect
-if ($lang != "json") $lang = "xml";
+
+try{
+    $utilisateur = $dao->getUnUtilisateur($pseudo);
+    $destinataire = $dao->getUnUtilisateur($pseudoDestinataire);
+    //echo $mailUtilisateur = $destinataire->getAdrMail();
+    $ok = Outils::envoyerMail($destinataire->getAdrMail(), "TraceGPS demande d'autorisation de ".$pseudo, $texteMessage."\n\nCliquer sur se lien pour accepter la demande :\nhttp://127.0.0.1/ws-php-mentec/tracegps%20kraken/tracegps/services/ValiderDemandeAutorisation.php?a=".$destinataire->getMdpsha1()."&b=".$pseudo."&c=".$pseudoDestinataire."&d=1\n\nCliquer sur se lien pour refuser la demande :\nhttp://127.0.0.1/ws-php-mentec/tracegps%20kraken/tracegps/services/ValiderDemandeAutorisation.php?a=".$destinataire->getMdpsha1()."&b=".$pseudo."&c=".$pseudoDestinataire."&d=0", $ADR_MAIL_EMETTEUR);
+    //echo $mail = $destinataire->getAdrMail() ." | ". " TraceGPS demande d autorisation de ".$pseudo." | ".$texteMessage." | ". $ADR_MAIL_EMETTEUR."<br>";
+}
+catch(Exception $e) {
+    $msg = "Erreur : données incomplètes.";
+    echo $e;
+}
+Outils::envoyerMail($destinataire->getAdrMail(), "TraceGPS demande d autorisation de "+$pseudo, $texteMessage, $ADR_MAIL_EMETTEUR);
+
+if ($lang != "json"){
+    $lang = "xml";
+}
 
 // Contrôle de la présence des paramètres
-if ( $pseudo == "") {
+
+    if( $pseudo == "" || $mdpSha1 == "" || $nomPrenom == "" || $pseudoDestinataire == "" || $texteMessage == "") {
     $msg = "Erreur : données incomplètes.";
-}
-else{
-   	// envoie un courriel  à l'utilisateur avec son nouveau mot de passe
-	$nouveauMdp = substr(sha1(microtime()),rand(0,26),15);
-   	$ok = $dao->envoyerMdp ($pseudo, $nouveauMdp);
-    if ( ! $ok ) {
-        $msg = "Enregistrement effectué ; l'envoi du courriel  de confirmation a rencontré un problème.";
     }
     else {
-		$msg = "Enregistrement effectué ; vous allez recevoir un courriel  de confirmation.";
-	}	
-}
+        if (! $utilisateur)
+        {
+            $msg = "Erreur : utilisateur inexistant.";
+        }
+        else {
+            if(! $destinataire)
+        {
+            $msg = "Erreur : destinataire inexistant.";
+        }
+        else {
+
+            if($ok){
+                $msg = $pseudoDestinataire+" va recevoir un courriel avec votre demande.";
+            }
+            else {
+                $msg = "Erreur : l'envoi du courriel de demande d'autorisation a rencontré un problème.";
+            }
+        }
+        }
+    }
+            
+
+
 
 // ferme la connexion à MySQL :
 unset($dao);
@@ -67,7 +101,7 @@ function creerFluxXML($msg)
 {	
     /* Exemple de code XML
         <?xml version="1.0" encoding="UTF-8"?>
-        <!--Service web DemanderMdp - BTS SIO - Lycée De La Salle - Rennes-->
+        <!--Service web DemanderUneAutorisation - BTS SIO - Lycée De La Salle - Rennes-->
         <data>
             <reponse>Erreur : authentification incorrecte.</reponse>
         </data>
