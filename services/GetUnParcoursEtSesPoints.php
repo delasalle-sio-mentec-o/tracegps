7a8f9b3b7e8396d0f31963eedc472b4193503161
delasalle-sio-mentec-o <delasalle.sio.mentec.o@gmail.com>
2018-12-04 16:45:59 +0100

    Modification affichage XML/JSON de GetUnParcoursEtSesPoints

--- services/GetUnParcoursEtSesPoints.php
+++ services/GetUnParcoursEtSesPoints.php
@@ -43,13 +43,13 @@ else
 	        }
 	        else {
 	            // suppression du parcours dans la BDD
-	            $ok = $dao->getUneTrace($idTrace);
-	            if ( $ok ) {
+	           $uneTrace = $dao->getUneTrace($idTrace);
+	           if ( $uneTrace ) {
 	                $msg = "Données de la trace demandée.";
                 }
                 else {
                     // tout a fonctionné
-                    $msg = "Erreur .";
+                    $msg = "Erreur : parcours inexistant.";
                 }
             }
 	    }
@@ -60,10 +60,10 @@ unset($dao);
 
 // création du flux en sortie
 if ($lang == "xml") {
-    creerFluxXML($msg);
+    creerFluxXML($msg, $uneTrace);
 }
 else {
-    creerFluxJSON($msg);
+    creerFluxJSON($msg, $uneTrace);
 }
 
 // fin du programme (pour ne pas enchainer sur la fonction qui suit)
@@ -72,48 +72,148 @@ exit;
 
 
 // création du flux XML en sortie
-function creerFluxXML($msg)
-{	// crée une instance de DOMdocument (DOM : Document Object Model)
-	$doc = new DOMDocument();
-	
-	// specifie la version et le type d'encodage
-	$doc->version = '1.0';
-	$doc->encoding = 'UTF-8';
-	
-	// crée un commentaire et l'encode en UTF-8
-	$elt_commentaire = $doc->createComment('Service web SupprimerUnParcours - BTS SIO - Lycée De La Salle - Rennes');
-	// place ce commentaire à la racine du document XML
-	$doc->appendChild($elt_commentaire);
-	
-	// crée l'élément 'data' à la racine du document XML
-	$elt_data = $doc->createElement('data');
-	$doc->appendChild($elt_data);
-	
-	// place l'élément 'reponse' dans l'élément 'data'
-	$elt_reponse = $doc->createElement('reponse', $msg);
-	$elt_data->appendChild($elt_reponse);
-
-	// Mise en forme finale
-	$doc->formatOutput = true;
-	
-	// renvoie le contenu XML
-	echo $doc->saveXML();
-	return;
-}
-
-// création du flux JSON en sortie
-function creerFluxJSON($msg)
-{
-    /* Exemple de code JSON
-         {
-            "data": {
-                "reponse": "Erreur : authentification incorrecte."
-            }
-         }
-     */
+function creerFluxXML($msg, $uneTrace)
+{	    // crée une instance de DOMdocument (DOM : Document Object Model)
+    $doc = new DOMDocument();
     
-    // construction de l'élément "data"
-    $elt_data = ["reponse" => $msg];
+    // specifie la version et le type d'encodage
+    $doc->version = '1.0';
+    $doc->encoding = 'UTF-8';
+    
+    // crée un commentaire et l'encode en UTF-8
+    $elt_commentaire = $doc->createComment('Service web GetTousLesUtilisateurs - BTS SIO - Lycée De La Salle - Rennes');
+    // place ce commentaire à la racine du document XML
+    $doc->appendChild($elt_commentaire);
+    
+    // crée l'élément 'data' à la racine du document XML
+    $elt_data = $doc->createElement('data');
+    $doc->appendChild($elt_data);
+    
+    // place l'élément 'reponse' dans l'élément 'data'
+    $elt_reponse = $doc->createElement('reponse', $msg);
+    $elt_data->appendChild($elt_reponse);
+    
+    
+    
+    
+    // traitement des utilisateurs
+    if (sizeof($uneTrace) > 0) {
+        // place l'élément 'donnees' dans l'élément 'data'
+        $elt_donnees = $doc->createElement('donnees');
+        $elt_data->appendChild($elt_donnees);
+        
+        // place l'élément 'lesUtilisateurs' dans l'élément 'donnees'
+        $elt_lesParcoursDunUtilisateur = $doc->createElement('t');
+        //$elt_donnees->appendChild($elt_lesParcoursDunUtilisateur);
+        
+        // crée un élément vide 'utilisateur'
+        $elt_ParcourDunUtilisateur = $doc->createElement('trace');
+        // place l'élément 'utilisateur' dans l'élément 'lesUtilisateurs'
+        $elt_donnees->appendChild($elt_ParcourDunUtilisateur);
+        
+        // crée les éléments enfants de l'élément 'utilisateur'
+        $elt_id         = $doc->createElement('id', $uneTrace->getId());
+        $elt_ParcourDunUtilisateur->appendChild($elt_id);
+        
+        $elt_dateHeureDebut    = $doc->createElement('dateHeureDebut', $uneTrace->getDateHeureDebut());
+        $elt_ParcourDunUtilisateur->appendChild($elt_dateHeureDebut);
+        
+        $elt_terminee    = $doc->createElement('terminee', $uneTrace->getTerminee());
+        $elt_ParcourDunUtilisateur->appendChild($elt_terminee);
+        if ($uneTrace->getDateHeureFin() != null)
+        {
+            $elt_dateHeureFin     = $doc->createElement('dateHeureFin', $uneTrace->getDateHeureFin());
+            $elt_ParcourDunUtilisateur->appendChild($elt_dateHeureFin);
+        }
+
+        
+        $elt_idUtilisateur = $doc->createElement('idUtilisateur', $uneTrace->getIdUtilisateur());
+        $elt_ParcourDunUtilisateur->appendChild($elt_idUtilisateur);
+        
+        $elt_PointUneTrace = $doc->createElement('lesPoints');
+        $elt_donnees->appendChild($elt_PointUneTrace);
+        
+            
+        foreach ($uneTrace->getLesPointsDeTrace() as $unParcourDunUtilisateur)
+        {
+            // crée un élément vide 'utilisateur'
+            $elt_ParcourDunUtilisateur = $doc->createElement('point');
+            $elt_PointUneTrace->appendChild($elt_ParcourDunUtilisateur);
+            
+            
+            
+            // crée les éléments enfants de l'élément 'utilisateur'
+            $elt_id         = $doc->createElement('id', $unParcourDunUtilisateur->getId());
+            $elt_ParcourDunUtilisateur->appendChild($elt_id);
+            
+            $elt_latitude   = $doc->createElement('latitude', $unParcourDunUtilisateur->getLatitude());
+            $elt_ParcourDunUtilisateur->appendChild($elt_latitude);
+            
+            $elt_longitude    = $doc->createElement('longitude', $unParcourDunUtilisateur->getLongitude());
+            $elt_ParcourDunUtilisateur->appendChild($elt_longitude);
+            
+            $elt_altitude     = $doc->createElement('altitude', $unParcourDunUtilisateur->getAltitude());
+            $elt_ParcourDunUtilisateur->appendChild($elt_altitude);
+            
+            $elt_dateHeure     = $doc->createElement('dateHeure', $unParcourDunUtilisateur->getDateHeure());
+            $elt_ParcourDunUtilisateur->appendChild($elt_dateHeure);
+            
+            $elt_rythmeCardio = $doc->createElement('rythmeCardio', $unParcourDunUtilisateur->getRythmeCardio());
+            $elt_ParcourDunUtilisateur->appendChild($elt_rythmeCardio);
+        }
+    }
+    // Mise en forme finale
+    $doc->formatOutput = true;
+    
+    // renvoie le contenu XML
+    echo $doc->saveXML();
+    return;
+}
+function creerFluxJSON($msg, $uneTrace)
+{
+    if (sizeof($uneTrace) == 0) {
+        // construction de l'élément "data"
+        $elt_data = ["reponse" => $msg];
+    }
+    else {
+        // construction d'un tableau contenant les utilisateurs
+        $lesObjetsDuTableau = array();
+        
+        $unObjetTrace = array();
+        $lesObjetsDuTableauTrace = array();
+        
+        $unObjetParcourDunUtilisateur = array();
+        $unObjetTrace["id"] = $uneTrace->getId();
+        $unObjetTrace["dateHeureDebut"] = $uneTrace->getDateHeureDebut();
+        $unObjetTrace["terminee"] = $uneTrace->getTerminee();
+        if ($uneTrace->getDateHeureFin() != null)
+            $unObjetTrace["dateHeureFin"] = $uneTrace->getDateHeureFin();
+            
+        $unObjetTrace["idUtilisateur"] = $uneTrace->getIdUtilisateur();
+        $lesObjetsDuTableauTrace[] = $unObjetTrace;
+        
+        foreach ($uneTrace->getLesPointsDeTrace() as $unParcourDunUtilisateur)
+        {	// crée une ligne dans le tableau
+            
+            $unObjetParcourDunUtilisateur = array();
+            $unObjetParcourDunUtilisateur["id"] = $unParcourDunUtilisateur->getId();
+            $unObjetParcourDunUtilisateur["latitude"] = $unParcourDunUtilisateur->getLatitude();
+            $unObjetParcourDunUtilisateur["longitude"] = $unParcourDunUtilisateur->getLongitude();
+            $unObjetParcourDunUtilisateur["altitude"] = $unParcourDunUtilisateur->getAltitude();
+                
+            $unObjetParcourDunUtilisateur["dateHeure"] = $unParcourDunUtilisateur->getDateHeure();
+            $unObjetParcourDunUtilisateur["rythmeCardio"] = $unParcourDunUtilisateur->getRythmeCardio();
+            $lesObjetsDuTableau[] = $unObjetParcourDunUtilisateur;
+        }
+        // construction de l'élément "lesTraces"
+        
+        
+        $elt_ParcoursDunUtilisateur = ["trace" => $lesObjetsDuTableauTrace, "lesPoints" => $lesObjetsDuTableau];
+        
+        
+        // construction de l'élément "data"
+        $elt_data = ["reponse" => $msg, "donnees" => $elt_ParcoursDunUtilisateur];
+    }
     
     // construction de la racine
     $elt_racine = ["data" => $elt_data];
