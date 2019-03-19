30d32b251910a7c40f3c1625ba201b13335ed0c4
Unknown <delasalle.sio.derrien.r@gmail.com>
2019-03-19 14:22:23 +0100

    Correction accès bdd wamp1

--- modele/parametres.php
+++ modele/parametres.php
@@ -6,11 +6,11 @@
 
 // paramètres de connexion -----------------------------------------------------------------------------------
 global $PARAM_HOTE, $PARAM_PORT, $PARAM_BDD, $PARAM_USER, $PARAM_PWD;
-$PARAM_HOTE = "localhost";		// si le sgbd est sur la même machine que le serveur php
+$PARAM_HOTE = "serv-wamp1";		// si le sgbd est sur la même machine que le serveur php
 $PARAM_PORT = "3306";			// le port utilisé par le serveur MySql
-$PARAM_BDD = "tracegps";		// nom de la base de données
-$PARAM_USER = "tracegps";		// nom de l'utilisateur
-$PARAM_PWD = "spgecart";		// son mot de passe
+$PARAM_BDD = "tracegps07";		// nom de la base de données
+$PARAM_USER = "Derrien";		// nom de l'utilisateur
+$PARAM_PWD = "derrien";		// son mot de passe
 
 // Autres paramètres -----------------------------------------------------------------------------------------
 
